acce090831358e05ca19743477e39ce39a163675
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2026-03-23 10:10:38 +0000

    chore: add scaffold of components

---
+++ src/components/TodoCard/TodoCard.jsx
@@ -0,0 +1,11 @@
+import "./TodoCard.css";
+
+const TodoCard = () => {
+  return (
+    <>
+      <h2>Hello from TodoCard</h2>
+    </>
+  );
+};
+
+export default TodoCard;
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,12 @@
+import TodoCard from "../TodoCard/TodoCard";
+import "./TodoList.css";
+const TodoList = () => {
+  return (
+    <>
+      <h2>Hello from TodoList</h2>
+      <TodoCard />
+    </>
+  );
+};
+
+export default TodoList;
